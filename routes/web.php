839de7225ff869c9483daedba0ee1a0777f1b644
weasiser <1469115803@qq.com>
2019-06-19 10:45:25 +0800

    web route simplify

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Auth::routes(['register' => false]);
+//Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');

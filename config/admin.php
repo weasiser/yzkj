3a9b636a09b273899bb2a2d16963f856753e9ea1
weasiser <1469115803@qq.com>
2019-06-25 18:13:24 +0800

    oss配置

--- config/admin.php
+++ config/admin.php
@@ -151,7 +151,7 @@ return [
     'upload' => [
 
         // Disk in `config/filesystem.php`.
-        'disk' => 'admin',
+        'disk' => env('FILESYSTEM_DRIVER', 'admin'),
 
         // Image and file upload path under the disk above.
         'directory' => [
--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Jacobcyl\AliOSS\AliOssServiceProvider::class,
+
     ],
 
     /*

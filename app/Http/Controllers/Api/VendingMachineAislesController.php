0759fbfab79ae66de58867413562fd4df1605f5a
weasiser <1469115803@qq.com>
2019-07-06 12:37:16 +0800

    替换商品

--- app/Http/Controllers/Api/VendingMachineAislesController.php
+++ app/Http/Controllers/Api/VendingMachineAislesController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\VendingMachineAisle;
+use App\Transformers\ProductTransformer;
 use App\Transformers\VendingMachineAisleTransformer;
 use Illuminate\Http\Request;
 
 class VendingMachineAislesController extends Controller
 {
-    public function update(VendingMachineAisle $vendingMachineAisle, Request $request, VendingMachineAisleTransformer $vendingMachineAisleTransformer)
+    public function update(VendingMachineAisle $vendingMachineAisle, Request $request, VendingMachineAisleTransformer $vendingMachineAisleTransformer, ProductTransformer $productTransformer)
     {
         if ($request->input('is_opened') === 'change') {
             $vendingMachineAisle->is_opened = !$vendingMachineAisle->is_opened;
@@ -17,6 +18,11 @@ class VendingMachineAislesController extends Controller
             $vendingMachineAisle->increaseStock();
         } elseif ($request->input('stock') === 'minus') {
             $vendingMachineAisle->decreaseStock();
+        } elseif ($product_id = $request->input('product_id')) {
+            $vendingMachineAisle->product_id = $product_id;
+            $vendingMachineAisle->update();
+            $product = $vendingMachineAisle->product;
+            return $this->response->item($product, $productTransformer);
         }
 //        return $this->response->item($vendingMachineAisle, $vendingMachineAisleTransformer);
 //        return $this->response->array([

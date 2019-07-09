e2978c4827272e25c4c6845bb01ba034b264ce49
weasiser <1469115803@qq.com>
2019-07-09 18:15:10 +0800

    wxpay

--- config/pay.php
+++ config/pay.php
@@ -23,17 +23,17 @@ return [
     ],
 
     'wxpay' => [
-        'appid' => 'wxb3fxxxxxxxxxxx', // APP APPID
-        'app_id' => 'wxb3fxxxxxxxxxxx', // 公众号 APPID
-        'miniapp_id' => 'wxb3fxxxxxxxxxxx', // 小程序 APPID
-        'mch_id' => '14577xxxx',
-        'key' => 'mF2suE9sU6Mk1Cxxxxxxxxxxx',
-        'notify_url' => 'http://yanda.net.cn/notify.php',
-        'cert_client' => './cert/apiclient_cert.pem', // optional，退款等情况时用到
-        'cert_key' => './cert/apiclient_key.pem',// optional，退款等情况时用到
+//        'appid' => 'wxb3fxxxxxxxxxxx', // APP APPID
+//        'app_id' => 'wxb3fxxxxxxxxxxx', // 公众号 APPID
+        'miniapp_id' => env('WXPAY_MINI_APP_ID'), // 小程序 APPID
+        'mch_id' => env('WXPAY_MCH_ID'),
+        'key' => env('WXPAY_KEY'),
+        'notify_url' => env('WXPAY_NOTIFY_URL'),
+        'cert_client' => resource_path('wxpay/apiclient_cert.pem'),//'./cert/apiclient_cert.pem', // optional，退款等情况时用到
+        'cert_key' => resource_path('wxpay/apiclient_key.pem'),//'./cert/apiclient_key.pem',// optional，退款等情况时用到
         'log' => [ // optional
-            'file' => './logs/wechat.log',
-            'level' => 'info', // 建议生产环境等级调整为 info，开发环境为 debug
+            'file' => storage_path('logs/wechat_pay.log'),
+            'level' => 'debug', // 建议生产环境等级调整为 info，开发环境为 debug
             'type' => 'single', // optional, 可选 daily.
             'max_file' => 30, // optional, 当 type 为 daily 时有效，默认 30 天
         ],

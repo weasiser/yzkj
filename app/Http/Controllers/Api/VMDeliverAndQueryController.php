60c688625dcbcdcd7272b4e92400337007672aa0
weasiser <1469115803@qq.com>
2019-07-16 16:04:13 +0800

    出货

--- app/Http/Controllers/Api/VMDeliverAndQueryController.php
+++ app/Http/Controllers/Api/VMDeliverAndQueryController.php
@@ -6,6 +6,7 @@ use App\Handlers\VendingMachineDeliverAndQuery;
 use App\Jobs\DeliverProduct;
 use App\Models\VendingMachine;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class VMDeliverAndQueryController extends Controller
 {

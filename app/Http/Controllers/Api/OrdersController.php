5dcf7a8bab276245957897423787a12c2ebf2b24
weasiser <1469115803@qq.com>
2019-08-24 10:32:27 +0800

    test load more finish

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -96,7 +96,7 @@ class OrdersController extends Controller
     {
         $user = $this->user();
 
-        $orders = $user->orders()->recent()->paginate(3);
+        $orders = $user->orders()->recent()->paginate(5);
 
         return $this->response->paginator($orders, $orderTransformer);
     }

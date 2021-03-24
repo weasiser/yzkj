a1f6f2241bf8e27cdcb1c0b435b4d4c12840fb00
weasiser <1469115803@qq.com>
2021-03-24 12:52:59 +0800

    available vm stock

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -16,7 +16,7 @@ class ProductsController extends Controller
 
     public function getAvailableProductStock(Product $product)
     {
-        $availableProductStock = $product->where('products.on_sale', true)->selectRaw('products.title, products.image, products.vending_machine_stock, sum(vending_machine_aisles.max_stock - vending_machine_aisles.stock) as available_product_stock')->leftjoin('vending_machine_aisles', 'products.id', '=', 'vending_machine_aisles.product_id')->leftjoin('vending_machines', 'vending_machine_aisles.vending_machine_id', '=', 'vending_machines.id')->where('vending_machines.is_opened', true)->groupBy('products.id')->orderBy('products.id', 'desc')->get();
+        $availableProductStock = $product->where('products.on_sale', true)->selectRaw('products.title, products.image, sum(vending_machine_aisles.max_stock - vending_machine_aisles.stock) as available_product_stock, sum(vending_machine_aisles.stock) as vending_machine_stock')->leftjoin('vending_machine_aisles', 'products.id', '=', 'vending_machine_aisles.product_id')->leftjoin('vending_machines', 'vending_machine_aisles.vending_machine_id', '=', 'vending_machines.id')->where('vending_machines.is_opened', true)->groupBy('products.id')->orderBy('products.id', 'desc')->get();
         foreach ($availableProductStock as $value) {
             $value['image'] = config('filesystems.disks.oss.cdnDomain') ? config('filesystems.disks.oss.cdnDomain') . '/' . $value['image'] . '-product' : Storage::disk(config('admin.upload.disk'))->url($value['image']) . '-product';
         }
